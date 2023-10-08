ef29783c1d4a9c0bcf3e9dc9a72f949f9080b82f
Anirudh Ravishankar <55047320+anirudhbalram@users.noreply.github.com>
2023-10-08 19:25:07 +0200

    Add files via upload

--- half_radii_z_2.sh
+++ half_radii_z_2.sh
@@ -2,16 +2,16 @@
 #SBATCH --qos=narayanan-b
 #SBATCH --job-name=half_radii_2    # Job name
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=ranirudh@students.iisertirupati.ac.in     # Where to send mail
+#SBATCH --mail-user=     # Where to send mail
 #SBATCH --nodes=1                    # Run all processes on a single node       
 #SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --array=1-268
+#SBATCH --array=1-268    # number of cores = number of galaxies run by slick for this particular snapshot
 #SBATCH --mem=80gb                     # Job memory request
-#SBATCH --time=48:00:00               # Time limit hrs:min:sec
-#SBATCH --output=half_radii_2.log   # Standard output and error log
+#SBATCH --time=02:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/dev/null   # Standard output and error log
 
 module load conda
 
 conda activate /blue/narayanan/a.ravishankar/py38
 
-python half_radii_z_2_fix.py --galinfoline $SLURM_ARRAY_TASK_ID
+python half_radii_z_2.py --galinfoline $SLURM_ARRAY_TASK_ID
