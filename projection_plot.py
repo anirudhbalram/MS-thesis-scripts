8f5a32010c86bc9268d4cf581e7a87376ef38e5f
Anirudh Ravishankar <55047320+anirudhbalram@users.noreply.github.com>
2023-10-08 19:26:34 +0200

    Delete projection_plot.py

--- projection_plot.py
+++
@@ -1,7 +0,0 @@
-import yt
-
-snap = '/orange/narayanan/desika.narayanan/gizmo_runs/simba/m25n512/output/snapshot_150.hdf5'
-ds  = yt.load(snap)
-
-p = yt.ProjectionPlot(ds, "z", ("gas", "temperature"))
-p.save("plot_snapshot_150_gas_temperature.jpg")
